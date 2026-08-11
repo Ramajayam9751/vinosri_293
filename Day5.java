2c4d56d95c245c495c1b1875639303632f2b3ff9
Ramajayam9751 <vinosri162@gmail.com>
2026-08-11 10:12:39 +0530

    added

--- Day5.java
+++ Day5.java
@@ -1,4 +1,102 @@
 public class Day5 {
      
+    public static void main(String[]args){
+
+    // String
     
+   // String name="vinosri";
+   // System.out.println(name);
+   // String welcome="credo system";
+   // System.out.println(welcome);
+
+   //length();
+
+  // String name="vinosri";
+  // System.out.println(name.length());
+  // String welcome="credo system";
+  // System.out.println(welcome.length());
+
+ // CharAt();
+
+// String name="vinosri";
+// System.out.println(name.charAt(5));
+//String welcome="credo system";
+// System.out.println(welcome.charAt(8));
+
+//toupperCase();
+
+//String name="vinosri";
+//System.out.println(name.toUpperCase());
+//System.out.println(name.toLowerCase());
+
+//equal();
+//String T1="VINOSRI";
+//String T2="vinosri";
+//System.out.println(T1.equals(T2));
+//String a1="credo";
+//String a2="credo";
+//System.out.println(a1.equals(a2));
+
+//equalsIgnoreCase();
+
+//String a1="system";
+//String a2="system";
+//System.out.println(a1.equalsIgnoreCase(a2));
+//String a1="credo";
+//String a2="system";
+//System.out.println(a1.equalsIgnoreCase(a2));
+
+//Contains();
+//String name="vinosri";
+//System.out.println(name.contains("r"));
+//String name="credo";
+//System.out.println(name.contains("e"));
+
+//Startwidth(); endwidth();
+//String name="vinosri";
+//System.out.println(name.startsWith("v"));
+//System.out.println(name.endsWith("r"));
+//System.out.println(name.endsWith("i"));
+
+//indexof(); lastIndexof();------------------------------->
+//String name="vinosri";
+//System.out.println(name.indexOf("r"));
+//System.out.println(name.lastIndexOf("i"));
+
+//Replace---------------------------->
+//String check="call cat";
+//System.out.println(check.replace("c", "b"));
+//String check="shall";
+//System.out.println(check.replace("s", "b"));
+
+//trim()------------------------------------>
+//String check="             vinosri             ";
+//System.out.println(check.trim());
+
+//Concat();---------------------------->
+//String a1="welcome";
+//String a2="credo system";
+//System.out.println(a1.concat(a2));
+
+//Math and all methods------------------------------->
+//abs();
+//int a=50;
+//int b=40;
+//System.out.println(Math.abs(50-40));
+//System.out.println(50-40);
+
+//Max(); Min();----------------------------->
+//int a=45;
+//int b=35;
+//System.out.println(Math.max(a, b));
+//System.out.println(Math.min(a,b));
+
+//Sqrt()----------------------->
+//int a=64;
+//System.out.println(Math.sqrt(64));
+
+//Power()--------------------->
+
+
+    }
 }
