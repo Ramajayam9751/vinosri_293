84d02c81abcb33ce1d0abc3371ad48976117b536
Ramajayam9751 <vinosri162@gmail.com>
2026-08-14 17:21:36 +0530

    added

---
+++ Day7.java
@@ -0,0 +1,39 @@
+public class Day7 {
+    public void display(){
+      //  System.out.println("welcome to credo systemz");
+
+     // Without arg,without return type---------------->
+    // static void add();
+    // int a=10;
+     //int b=20;
+    // System.out.println(a+b);
+
+     // With arh,without return type----------------->
+    // static void add(int a,int b,int c){
+    //    System.out.println(a+b+c);
+    // }
+ 
+
+    // Without arg,with return type-------------------->
+   // static int add(){
+     //   int a=30;
+      //  int b=200;
+      //  return a+b;
+       // System.out.println(add());
+    //}
+
+   // with arg,with return type------------------------>
+   // static int add(int a,int b,int c,int d,int e){
+     //   return a+b+c+d+e;
+    }
+
+    }
+    public static void main(String[] args) {
+    //  display();
+    //  add();
+    //  add(20,100,600);
+    // System.out.println(add());
+     //   System.out.println(add(90,56,78,34,34));
+    }
+    
+}
