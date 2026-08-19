0904da6c8ce59e2f89696672cd2d7497f77e72e8
Ramajayam9751 <vinosri162@gmail.com>
2026-08-19 08:48:46 +0530

    added

---
+++ Day8.java
@@ -0,0 +1,89 @@
+public class Day8 {
+    
+    public static void main(String[] args) {
+            
+ // Access Array element
+       // int[] marks={10,20,30,40,50};
+       // System.out.println(marks[2]);
+       // System.out.println(marks[1]);
+       // System.out.println(marks[0]);
+       // System.out.println(marks[4]);
+
+// Change the element
+       //int[] marks={26,53,75,65,85};
+       //System.out.println(marks[3]);
+      // marks[3] =30;
+      // System.out.println(marks[3]);
+
+      //int[]marks={643,75,937};
+      //System.out.println(marks[1]);
+      //marks[1]=344;
+     // System.out.println(marks[1]);
+
+//using the for loop
+     // int[] marks={35,75,84,67,675};
+      //  for(int i=0; i< marks.length;i++){
+    // System.out.println(marks[i]);
+       // }
+       // string
+      // String[] names={"vinosri", "preethi", "sriram", "shanthi"};
+      // for(int i=0; i < names.length; i++){
+      //      System.out.println(names[i]);
+      // }
+      // String[] names={"vino","prethi","sriram"};
+      // for(int i=0; i < names.length; i++){
+       // System.out.println(names[i]);
+      // }
+
+//sum of the Array element
+    //  int[] numbers={54,45,75,87,67,64};
+    //  int sum=0;
+    //  for( int i=0; i < numbers.length; i++){
+    //   sum=sum+numbers[i];
+    //    System.out.println( "price -" +sum);
+    //  }
+
+    //  int[] numbers={98,54,75,34,44,77,77};
+    //  int sum=0;
+    //  for(int i=0; i < numbers.length; i++){
+    //    sum=sum+numbers[i];
+     //   System.out.println("price -" +sum);
+    //  }
+    //  System.out.println("answer-" +sum);
+
+     // double average=(double) sum/numbers.length;
+     // System.out.println("SUM =" +sum);
+     // System.out.println("AVERAGE =" +average);
+
+//find the largest number
+
+    // int[] marks={44,67,23,98,8};
+    // int largest=marks[0];
+    // for(int i=0; i < marks.length; i++){
+   //     if(marks[i] >largest){
+    //      largest=marks[i];
+
+    //    }
+    // }
+    // System.out.println("largest =" +largest);
+
+//find the smallast number
+       
+      //  int[] numbers={34,87,75,42,8};
+      //  int smallast=numbers[0];
+      //     for(int i=0; i <numbers.length; i++){
+      //      if(numbers[i] < smallast){
+      //        smallast=numbers[i];
+      //      }
+      //     }
+      //  System.out.println("smallast =" + smallast);
+
+//Revers the array
+          
+            int[] numbers={76,9,5,97,53};
+               for(int i=0; i < numbers.length; i++);
+               
+       
+    }
+    
+}
